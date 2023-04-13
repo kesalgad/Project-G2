8c1a850bdabb0d9c2f46a52992469c3537db8bc7
kesalgad <kesalgad@microsoft.com>
2023-04-13 16:59:23 -0600

    0413_1659_Kevin

---
+++ DataManager.py
@@ -0,0 +1,38 @@
+class DataManager:
+    def __init__(self, inventarios_file, bodegas_file, distribuidores_file, entregas_file):
+        self.inventarios_file = inventarios_file
+        self.bodegas_file = bodegas_file
+        self.distribuidores_file = distribuidores_file
+        self.entregas_file = entregas_file
+
+    def read_inventarios(self):
+        # implementation of read_inventarios method
+        pass
+
+    def write_inventarios(self, data):
+        # implementation of write_inventarios method
+        pass
+
+    def read_bodegas(self):
+        # implementation of read_bodegas method
+        pass
+
+    def write_bodegas(self, data):
+        # implementation of write_bodegas method
+        pass
+
+    def read_distribuidores(self):
+        # implementation of read_distribuidores method
+        pass
+
+    def write_distribuidores(self, data):
+        # implementation of write_distribuidores method
+        pass
+
+    def read_entregas(self):
+        # implementation of read_entregas method
+        pass
+
+    def write_entregas(self, data):
+        # implementation of write_entregas method
+        pass
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import sys
 from ui import *
 from sqlite3 import *
 from PySide6 import QtCore, QtWidgets, QtGui
+from DataManager import DataManager
+
+
 
 class AppInventario(QMainWindow):
     def __init__(self):
@@ -9,6 +12,9 @@ class AppInventario(QMainWindow):
         self.ui = Ui_SistemaGestionInventarios()
         self.ui.setupUi(self)
         self.show()
+
+        # Create an instance of the DataManager class
+        self.dm = DataManager('inventarios.json', 'bodegas.json', 'distribuidores.json', 'entregas.json')
 
         # Mapeo de los botones a las páginas *"lambda" es una función anónima
         self.ui.pushButton_inicio.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
@@ -18,9 +24,26 @@ class AppInventario(QMainWindow):
         self.ui.pushButton_ent.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(4))
         self.ui.pushButton_rep.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(5))
 
+        # Use the DataManager to read data
+        inventarios_data = self.dm.read_inventarios()
+        bodegas_data = self.dm.read_bodegas()
+        distribuidores_data = self.dm.read_distribuidores()
+        entregas_data = self.dm.read_entregas()
+
+        # Do something with the data
+        print(inventarios_data)
+        print(bodegas_data)
+        print(distribuidores_data)
+        print(entregas_data)
+
+        # Use the DataManager to write data
+        self.dm.write_inventarios(inventarios_data)
+        self.dm.write_bodegas(bodegas_data)
+        self.dm.write_distribuidores(distribuidores_data)
+        self.dm.write_entregas(entregas_data)
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = AppInventario()
     window.show()
     sys.exit(app.exec_())
-    
