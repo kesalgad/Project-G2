a61c147f40e6fff643ab9ee27a419f31774e8149
Kevin Salgado <kev.salgado@icloud.com>
2023-04-15 23:27:35 -0600

    0415-2327-Kevin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 from ui import *
 from PySide6 import QtCore, QtWidgets, QtGui
-from Unused_Files.DataManager import DataManager
-from InventoryItem import *
 
 
 
@@ -13,16 +11,6 @@ class AppInventario(QMainWindow):
         self.ui = Ui_SistemaGestionInventarios()
         self.ui.setupUi(self)
 
-        # Create an instance of InventoryItemUI and add it to the layout of the
-        # Inventarios page in the stackedWidget
-        self.inventory_item_ui = InventoryItemUI()
-        self.ui.Inventarios.layout().addWidget(self.inventory_item_ui)
-
-
-
-        # Create an instance of the DataManager class
-        self.dm = DataManager('inventarios.json', 'bodegas.json', 'distribuidores.json', 'entregas.json')
-
         # Mapeo de los botones a las páginas *"lambda" es una función anónima
         self.ui.pushButton_inicio.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
         self.ui.pushButton_inv.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))
@@ -30,10 +18,7 @@ class AppInventario(QMainWindow):
         self.ui.pushButton_dist.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(3))
         self.ui.pushButton_ent.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(4))
         self.ui.pushButton_rep.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(5))
-        
-        # Create an instance of the InventoryItemUI class and connect it to the second page of the stacked widget
-        self.inventory_item_ui = InventoryItemUI()
-        #self.ui.Inventarios.layout().addWidget(self.inventory_item_ui)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
